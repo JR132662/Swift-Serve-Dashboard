e532ea13e0b06dd903bc7259b50f9af3dd15f1fe
Jonathan <cooljon08@gmail.com>
2025-11-30 14:06:34 -0500

    themeing issues solved 2

--- components/hero.tsx
+++ components/hero.tsx
@@ -50,13 +50,13 @@ export default function Hero() {
                 <div className="absolute inset-0 z-[1] pointer-events-none bg-background/75"></div>
                 <div
                     aria-hidden
-                    className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block">
+                    className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block overflow-hidden">
                     <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
                     <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
                     <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
                 </div>
-                <section className="relative z-10">
-                    <div className="relative pt-24 md:pt-36">
+                <section className="relative z-10 overflow-hidden">
+                    <div className="relative pt-24 md:pt-36 overflow-hidden">
                         <AnimatedGroup
                             variants={{
                                 container: {
@@ -180,7 +180,7 @@ export default function Hero() {
                                 },
                                 ...transitionVariants,
                             }}>
-                            <div className="mask-b-from-85% relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
+                            <div className="mask-b-from-85% relative mt-8 overflow-hidden px-2 sm:mt-12 md:mt-20 w-full">
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                     <Image
                                         className="bg-background aspect-15/8 relative rounded-2xl"
